46c9e8dc9f0e0ce0eb919fe1c6698e222152816b
gustavoghislandi <gugasouza3003@gmail.com>
2026-01-28 14:37:29 -0300

    React Server Components

---
+++ RSC_react_server_components.js
@@ -0,0 +1,11 @@
+// RSC significa “React Server Components”.
+
+// É um recurso do React que permite renderizar componentes no servidor, enviando apenas HTML + dados para o cliente, sem incluir o código JS desses componentes no bundle final.
+
+// Em poucas palavras:
+
+    // RSC = componentes React que rodam no servidor
+    // Cliente recebe menos JS
+    // Melhora performance e TTI
+
+// Ideal para apps grandes com muita renderização de conteúdo
