0212ebb898f0001145161f385b125f9f0bc79609
gustavoghislandi <gugasouza3003@gmail.com>
2026-02-11 15:16:23 -0300

    Modularizando para facilitar a compreensão

--- tutorial/tic-tac-toe/src/App.js
+++ tutorial/tic-tac-toe/src/App.js
@@ -1,80 +1,6 @@
 import { useState } from "react";
+import Board from "./components/Board";
 
-function Square({ value, onSquareClick }) {
-
-  return <button
-    className="square"
-    onClick={onSquareClick}
-  >
-    {value}
-  </button>
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-export function Board({ xIsNext, squares, onPlay }) {
-  // const [xIsNext, setXIsNext] = useState(true);
-  // const [squares, setSquares] = useState(Array(9).fill(null)); // Array(9).fill(null) creates an array with nine elements and sets each of them to null.
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
-  function handleClick(i) {
-    // Se squares[i] for 'truthy', só retorna. Ou calcula vencedor.
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquares = squares.slice(); //  slice sem argumentos cria uma cópia (uma "fatia inteira")
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    onPlay(nextSquares)
-  }
-
-  const board = [...Array(3)].map((_, row) => ( //  _ underscore é convenção de nome de parâmetro não usado.
-    <div className="board-row">
-      {[...Array(3)].map((square, col) => { // Spread é necessário para o array ter índices. Array(3) não funcionaria. Poderia ser Array(3).fill(null) ou [0, 1, 2].
-        const index = row * 3 + col; // 0 * 3 + 0; 0 * 3 + 1; 0 * 3 + 2; 1 * 3 + 0;...
-        return (
-          <Square
-            key={index}
-            value={squares[index]}
-            onSquareClick={() => handleClick(index)}
-          />
-        );
-      })}
-    </div>
-  ));
-
-  return <>
-    <div style={{margin:10}}>{status}</div>
-    {board}
-  </>;
-}
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]); // Perceba como [Array(9).fill(null)] é um array com um único item, o qual é em si um array de 9 nulls.
@@ -85,9 +11,7 @@ export default function Game() {
   const [isAscending, setIsAscending] = useState(true); // Criação de um estado para armazenar qual o estado da ordenação.
 
   const orderedHistory = isAscending ? history : [...history].reverse(); // variável intermediária que deixa o history intacto. Spread cria uma cópia.
-    // orderedHistory controla apenas a ordem de exibição. history nunca é mutado; a cópia evita efeitos colaterais no estado. Não mutar estado é regra estrutural do React, não só “boa prática”.
-
-
+  // orderedHistory controla apenas a ordem de exibição. history nunca é mutado; a cópia evita efeitos colaterais no estado. Não mutar estado é regra estrutural do React, não só “boa prática”.
 
   const moves = orderedHistory.map((squares, move) => {
 
---
+++ tutorial/tic-tac-toe/src/components/Board.jsx
@@ -0,0 +1,68 @@
+import Square from "./Square";
+
+export default function Board({ xIsNext, squares, onPlay }) {
+  // const [xIsNext, setXIsNext] = useState(true);
+  // const [squares, setSquares] = useState(Array(9).fill(null)); // Array(9).fill(null) creates an array with nine elements and sets each of them to null.
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(i) {
+    // Se squares[i] for 'truthy', só retorna. Ou calcula vencedor.
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice(); //  slice sem argumentos cria uma cópia (uma "fatia inteira")
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares)
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const board = [...Array(3)].map((_, row) => ( //  _ underscore é convenção de nome de parâmetro não usado.
+    <div className="board-row">
+      {[...Array(3)].map((square, col) => { // Spread é necessário para o array ter índices. Array(3) não funcionaria. Poderia ser Array(3).fill(null) ou [0, 1, 2].
+        const index = row * 3 + col; // 0 * 3 + 0; 0 * 3 + 1; 0 * 3 + 2; 1 * 3 + 0;...
+        return (
+          <Square
+            key={index}
+            value={squares[index]}
+            onSquareClick={() => handleClick(index)}
+          />
+        );
+      })}
+    </div>
+  ));
+
+  return <>
+    <div style={{ margin: 10 }}>{status}</div>
+    {board}
+  </>;
+}
