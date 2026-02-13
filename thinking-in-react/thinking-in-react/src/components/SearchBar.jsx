6f7deebca636198db4744d66eedf8d1ef55609e5
gustavoghislandi <gugasouza3003@gmail.com>
2026-02-13 15:48:46 -0300

    pequeno ajuste visual para ficar mais próximo do exemplo do site

--- thinking-in-react/thinking-in-react/src/components/SearchBar.jsx
+++ thinking-in-react/thinking-in-react/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ export default function SearchBar() {
   return (
     <form>
       <input type="text" placeholder="Search..." />
-      <label>
+      <label style={{display: "block", margin: "10px 0px"}}>
         <input type="checkbox" />
         {' '}
         Only show products in stock
