ca668c3663da449738283cb8fa93643056d31095
gustavoghislandi <gugasouza3003@gmail.com>
2026-02-15 18:08:27 -0300

    commit que tenho que deletar porque fiz alguam besteira

--- thinking-in-react/thinking-in-react/src/components/SearchBar.jsx
+++ thinking-in-react/thinking-in-react/src/components/SearchBar.jsx
@@ -1,9 +1,16 @@
-export default function SearchBar() {
+export default function SearchBar({filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange}) {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      <input 
+      type="text" 
+      value={filterText}
+      placeholder="Search..."
+      onChange={(e) => onFilterTextChange(e.target.value)} />
       <label style={{display: "block", margin: "10px 0px"}}>
-        <input type="checkbox" />
+        <input 
+        type="checkbox"
+        checked={inStockOnly}
+        onChange={(e) => onInStockOnlyChange(e.target.checked)} />
         {' '}
         Only show products in stock
       </label>
