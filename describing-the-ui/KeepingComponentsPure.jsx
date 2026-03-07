887c8dca85fc885ac60e89b1246b4cade3c4fdd0
gustavoghislandi <gugasouza3003@gmail.com>
2026-03-07 11:30:50 -0300

    Recaptulação sobre componentes puros.

--- describing-the-ui/KeepingComponentsPure.jsx
+++ describing-the-ui/KeepingComponentsPure.jsx
@@ -171,3 +171,21 @@
     // If some data changes in the middle of rendering a deep component tree, React can restart rendering without wasting time to finish the outdated render. Purity makes it safe to stop calculating at any time.
 
     // Every new React feature we’re building takes advantage of purity. From data fetching to animations to performance, keeping components pure unlocks the power of the React paradigm.
+
+//---------------
+
+    // Recap
+
+    // A component must be pure, meaning:
+
+        // It minds its own business. It should not change any objects or variables that existed before rendering.
+
+        // SAME INPUTS, SAME OUTPUT. Given the same inputs, a component should always return the same JSX.
+
+        // Rendering can happen at any time, so components should not depend on each others’ rendering sequence.
+
+        // You should not mutate any of the inputs that your components use for rendering. That includes props, state, and context. To update the screen, “set” state instead of mutating preexisting objects.
+
+        // Strive to express your component’s logic in the JSX you return. When you need to “change things”, you’ll usually want to do it in an event handler. As a last resort, you can useEffect.
+
+        // Writing pure functions takes a bit of practice, but it unlocks the power of React’s paradigm.
