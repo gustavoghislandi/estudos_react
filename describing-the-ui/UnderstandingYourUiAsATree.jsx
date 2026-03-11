f7ad7b6fa3158c9418d569a8c1e70e7c37b26937
gustavoghislandi <gugasouza3003@gmail.com>
2026-03-10 23:29:17 -0300

    React usa estruturas de árvore

---
+++ describing-the-ui/UnderstandingYourUiAsATree.jsx
@@ -0,0 +1,7 @@
+// Understanding Your UI as a Tree
+
+// Your UI as a tree
+
+// Like browsers and mobile platforms, React also uses tree structures to manage and model the relationship between components in a React app. These trees are useful tools to understand how data flows through a React app and how to optimize rendering and app size.
+
+// Ver https://react.dev/learn/understanding-your-ui-as-a-tree para visualizar a imagem da árvore.
